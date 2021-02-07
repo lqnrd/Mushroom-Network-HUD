65dda1d990a98ea739b774f70b3f58b5847cfba2
Chris Kader <chris.kader@gmail.com>
2021-02-07 16:36:44 -0600

    Remove unknown POIs, Color Nearest Name, Add Dist.

--- actions.init.lua
+++ actions.init.lua
@@ -25,20 +25,29 @@ if not aura_env.f then
   }
   f.ids = {}
   for i, v in pairs(t) do
-    f.ids[i] = f:CreateFontString(nil, "BACKGROUND", "GameFontNormal")
-    f.ids[i]:SetTextColor(1, 1, 1, 1)
+    local poiInfo, name = v[3] and v[4] and C_AreaPoiInfo.GetAreaPOIInfo(v[3], v[4])
+    if poiInfo then
+      f.ids[i] = f:CreateFontString(nil, "BACKGROUND", "GameFontNormal")
+      f.ids[i].dist = 0
+      f.ids[i].name = ""
+      f.ids[i]:SetTextColor(1, 1, 1, 1)
+    end
   end
   function f:UpdatePortalNames()
     for i, v in pairs(t) do
-      local poiInfo, name = v[3] and v[4] and C_AreaPoiInfo.GetAreaPOIInfo(v[3], v[4])
-      if aura_env.config[tostring(i)] and (aura_env.config[tostring(i)] ~= "") then
-        name = aura_env.config[tostring(i)]
-      elseif poiInfo then
-        name = poiInfo.name
-      else
-        name = GetSpellInfo(i)
+      if f.ids[i] then
+        local poiInfo, name = v[3] and v[4] and C_AreaPoiInfo.GetAreaPOIInfo(v[3], v[4])
+        if aura_env.config[tostring(i)] and (aura_env.config[tostring(i)] ~= "") then
+          name = aura_env.config[tostring(i)]
+        elseif poiInfo then
+          name = poiInfo.name
+        else
+          name = GetSpellInfo(i)
+        end
+        f.ids[i].name = name
+        f.ids[i]:SetText(f.ids[i].name or "???")
       end
-      f.ids[i]:SetText(name or "???")
+
     end
   end
   f.p = f:CreateFontString(nil, "BACKGROUND", "GameFontNormal")
@@ -52,11 +61,38 @@ if not aura_env.f then
     local s = sin(-angle/(2*PI)*360)
     local c = cos(-angle/(2*PI)*360)
     local x, y
-    for i, v in pairs(t) do
-      x, y = scaleX*(v[1]*self.size-self.size/2), -(v[2]*self.size-self.size/2)
-      self.ids[i]:SetPoint("CENTER", self, "CENTER", x*c-y*s, x*s+y*c)
-    end
     local pos = C_Map.GetPlayerMapPosition(1819,"player")
+    local tD = 0
+    for i, v in pairs(t) do
+      if f.ids[i] then
+        local tDist = ((v[1] - pos.x) ^ 2 + (v[2] - pos.y) ^ 2) ^ 0.5
+        f.ids[i].dist = tDist
+      end
+    end
+    local low = math.huge
+    local sDI
+    for i, v in pairs(t) do
+      if f.ids[i] then
+        local comp = f.ids[i].dist
+        if comp < low then
+          low = comp
+          sDI = i
+        end
+      end
+    end
+
+    for i, v in pairs(t) do
+      if f.ids[i] then
+        local nameString = f.ids[i].name .. "\n" .. string.format("%.2f", f.ids[i].dist * 10)
+        if i == sDI then
+          nameString = "|cFF00FF00" .. nameString .. "|r"
+          f.ids[i]:SetText(nameString)
+        end
+        f.ids[i]:SetText(nameString)
+        x, y = scaleX*(v[1]*self.size-self.size/2), -(v[2]*self.size-self.size/2)
+        self.ids[i]:SetPoint("CENTER", self, "CENTER", x*c-y*s, x*s+y*c)
+      end
+    end
     if pos then
       x, y = scaleX*(pos.x*self.size-self.size/2), -(pos.y*self.size-self.size/2)
       self.p:SetPoint("CENTER", self, "CENTER", x*c-y*s, x*s+y*c)
@@ -64,7 +100,7 @@ if not aura_env.f then
   end
   f:UpdatePositions(GetPlayerFacing() or 0)
   f:SetScript("OnUpdate", function(self)
-    self:UpdatePositions(GetPlayerFacing() or 0)
+      self:UpdatePositions(GetPlayerFacing() or 0)
   end)
 end
 aura_env.f:UpdateSize()
